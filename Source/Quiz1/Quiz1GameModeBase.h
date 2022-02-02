1fb13a55b1d9c4d8ca694d38d72cf71b3fdd8b1e
Saad Khattak <saadrustam@gmail.com>
2022-02-01 19:28:58 -0500

    My quiz commit

---
+++ Source/Quiz1/Quiz1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Quiz1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUIZ1_API AQuiz1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
